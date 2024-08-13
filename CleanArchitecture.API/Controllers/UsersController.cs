98490e563e03587fe86c422eb44c571a259a11d1
Marcos Vinicius <marcos.viniciusk19@gmail.com>
2024-08-13 00:34:59 -0300

    .NET - Usando a Clean Architecture - V

--- CleanArchitecture.API/Controllers/UsersController.cs
+++ CleanArchitecture.API/Controllers/UsersController.cs
@@ -1,4 +1,7 @@
 ﻿using CleanArchitecture.Aplication.UseCases.CreateUser;
+using CleanArchitecture.Aplication.UseCases.DeleteUser;
+using CleanArchitecture.Aplication.UseCases.GetAllUser;
+using CleanArchitecture.Aplication.UseCases.UpdateUser;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,7 +13,7 @@ public class UsersController : ControllerBase
 {
 	private readonly IMediator _mediator;
 
-	public UsersController(IMediator mediator) {  _mediator = mediator; }
+	public UsersController(IMediator mediator) { _mediator = mediator; }
 
 	[HttpPost]
 	public async Task<ActionResult<CreateUserResponse>> Create(CreateUserRequest request, CancellationToken cancellationToken)
@@ -22,6 +25,34 @@ public class UsersController : ControllerBase
 		//	return BadRequest(validationResult.Errors);
 
 		var response = await _mediator.Send(request, cancellationToken);
+		return Ok(response);
+	}
+
+	[HttpGet]
+	public async Task<ActionResult<List<GetAllUserResponse>>> GetAll(CancellationToken cancellationToken)
+	{
+		var response = await _mediator.Send(new GetAllUserRequest(), cancellationToken);
+		return Ok(response);
+	}
+
+	[HttpPut("{id}")]
+	public async Task<ActionResult<UpdateUserResponse>> Update(Guid id, UpdateUserRequest updateUserRequest, CancellationToken cancellationToken)
+	{
+		if (id != updateUserRequest.id)
+			return BadRequest();
+
+		var response = await _mediator.Send(updateUserRequest, cancellationToken);
+		return Ok(response);
+	}
+
+	[HttpDelete("{id}")]
+	public async Task<ActionResult> Delete(Guid? id, CancellationToken cancellationToken)
+	{
+		if(id == null)
+			return BadRequest();
+
+		var deleteUserRequest = new DeleteUserRequest(id.Value);
+		var response = await _mediator.Send(deleteUserRequest, cancellationToken);
 		return Ok(response);
 	}
 }
---
+++ CleanArchitecture.Aplication/UseCases/UpdateUser/UpdateUserRequest.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace CleanArchitecture.Aplication.UseCases.UpdateUser;
+
+public sealed record UpdateUserRequest(Guid id, string Email, string Name) : IRequest<UpdateUserResponse>;
